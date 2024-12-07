79e831bfd86cdeae26f8db9b8cd5a8e7273bfedd
Phantom <188271330+phantomspectrumcompressor91975@users.noreply.github.com>
2024-12-07 11:35:19 -0700

    Initial commit

---
+++ Firefox/NoComments.js
@@ -0,0 +1,54 @@
+// Handle Both Shorts And Normal Videos
+console.log("No Comments PFP's Loaded")
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256)
+    var y = Math.floor(Math.random() * 256)
+    var z = Math.floor(Math.random() * 256)
+    return "rgb(" + x + "," + y + "," + z + ")"
+}
+function createPFP(text) {
+    var canvas = document.createElement("canvas")
+    canvas.width = 40
+    canvas.height = 40
+    var ctx = canvas.getContext("2d")
+    ctx.fillStyle = random_bg_color()
+    // color background
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    // add character
+    ctx.font = "25px Roboto"
+    ctx.textAlign = "center"
+    ctx.fillStyle = "rgb(255,255,255)"
+    ctx.fillText(text, 20, 28)
+    var dataUrl = canvas.toDataURL()
+    canvas.remove()
+    return dataUrl
+}
+
+function HandleVideo() {
+    // observer
+    var VideoObserver = new MutationObserver(function(mutations, observer) {
+        
+    });
+    VideoObserver.observe(document.body, {childList: true, attributes: true});
+}
+
+function HandleShorts() {
+    // observer
+    var ShortsObserver = new MutationObserver(function(mutations, observer) {
+        
+    });
+    ShortsObserver.observe(document, {childList: true, attributes: true});
+}
+
+window.onload = function() {
+    // check if shorts or normal video
+    if (document.location.href.includes("/watch?v=")) {
+        // Is Normal Video
+        HandleVideo()
+    } else if (document.location.href.includes("/shorts/")) {
+        // Is Short Form Video
+        HandleShorts()
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # No-Comment-Profile-Pictures
 Replaces all comment profile pictures with a randomly generated one
+
+[Chrome](https://chromewebstore.google.com/detail/)<br>
+[Firefox](https://addons.mozilla.org/en-CA/firefox/addon/)<br>
